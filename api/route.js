7aeea75003c16c6b05ade29632cee74469a9c881
Andreas <AndreasPapaz@gmail.com>
2017-09-20 10:58:24 -0500

    added yoda

--- api/route.js
+++ api/route.js
@@ -1,5 +1,6 @@
 const Client = require('node-rest-client').Client;
 const client = new Client();
+const unirest = require('unirest');
 
 
 module.exports = function(app) {
@@ -24,7 +25,7 @@ module.exports = function(app) {
 	app.post('/sms', function(req, res) {
 		const inMessage = req.body.Body;
 		const inMessageArr = inMessage.split(' ');
-		console.log(req.body);
+		// console.log(req.body);
 
 		switch(inMessageArr[0].toUpperCase()) {
 			case 'START':
@@ -32,6 +33,9 @@ module.exports = function(app) {
 				break;
 			case 'STOCK':
 				res.redirect('/stockData/' + inMessageArr[1]);
+				break;
+			case 'YODA':
+				res.redirect('yodaData/' + inMessageArr);
 				break;
 			default:
 				const twilio = require('twilio');
@@ -56,6 +60,19 @@ module.exports = function(app) {
 		});
 	});
 
+	app.post('/yodaData/:speakID', function(req, res) {
+		const translate1 = req.params.speakID;
+		const arr = translate1.split(',');
+
+		yodaSpeak(arr).then(function(data) {
+			const twilio = require('twilio');
+			const twiml = new twilio.twiml.MessagingResponse();
+			twiml.message(data);
+			res.writeHead(200, {'Content-Type' : 'text/xml'});
+			res.end(twiml.toString());
+		});
+	});
+
 	function getStock(stockId) {
 		return new Promise(function(resolve, reject) {
 			console.log(stockId + ' is this what you are looking for');
@@ -70,5 +87,22 @@ module.exports = function(app) {
 				resolve(infoSet);
 			});
 		})
+	}
+
+	function yodaSpeak(translate) {
+		return new Promise(function(resolve, reject) {
+			console.log(translate);
+			translate.shift();
+			translate.join(' ');
+			// console.log(translate.join('+'));
+			const query = 'https://yoda.p.mashape.com/yoda?sentence=' + translate.join('+');
+
+			unirest.get(query)
+			.header("X-Mashape-Key", "6e6ujaxTzwmshh0v75rUM84U6bUjp1v2wfDjsniJD4Nl70gu9F")
+			.header("Accept", "text/plain")
+			.end(function (result) {
+			  resolve(result.body);
+			});
+		});
 	}
 }
